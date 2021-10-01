413bd064839a5809c215a366a236f5b3f423deaf
Carmine <carmine.choi@gmail.com>
2021-09-30 23:25:34 -0700

    bottom bar created

--- src/renderer/NotePad.tsx
+++ src/renderer/NotePad.tsx
@@ -1,27 +1,41 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import './NotePad.global.css';
 
-import './NotePad.css';
+const CoordinateBar = () => {
+  return (
+    <div className="coordinate-bar">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
 
 const NotePad = () => {
   const [textValue, setTextValue] = useState('');
   const [selectionStart, setSelectionStart] = useState(0);
 
   return (
-    <div className="text-container">
-      <textarea
-        className="textArea"
-        value={textValue}
-        onChange={(e) => {
-          setTextValue(e.target.value);
-        }}
-        onClick={(e) => {
-          setSelectionStart(e.currentTarget.selectionStart);
-          console.log(selectionStart);
-        }}
-        spellCheck="false"
-      />
+    <div>
+      <div className="text-container">
+        <textarea
+          className="text-area"
+          value={textValue}
+          onChange={(e) => {
+            setTextValue(e.target.value);
+          }}
+          onClick={(e) => {
+            setSelectionStart(e.currentTarget.selectionStart);
+            console.log(selectionStart);
+          }}
+          spellCheck="false"
+        />
+      </div>
+      <CoordinateBar />
     </div>
   );
 };
